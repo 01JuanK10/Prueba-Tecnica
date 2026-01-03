f00b108e1d9963c02e67b073348e4fb9b0508be8
01JuanK10 <camiloalzatebedoya15@gmail.com>
2026-01-03 10:22:03 -0500

    Primera implementacion detalles podcast

--- src/app/components/detalles-podcast/detalles-podcast.ts
+++ src/app/components/detalles-podcast/detalles-podcast.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ServicioHttp } from '../../services/servicio-http';
+import { Podcast } from '../../domain/Podcast';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-detalles-podcast',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './detalles-podcast.html',
   styleUrl: './detalles-podcast.css',
 })
-export class DetallesPodcast {
+export class DetallesPodcast implements OnInit {
 
+  http = inject(ServicioHttp);
+
+  podcast!: Podcast;
+
+  constructor(){
+    this.podcast = {
+      id: '',
+      urlImagen: '',
+      titulo: '',
+      autor: '',
+      descripcion: '',
+    };
+  }
+
+  ngOnInit(): void {
+    this.http.obtenerDetallesPodcast('1535809341').subscribe((data) => {
+      console.log(data);
+      this.podcast = {
+        id: data.results[0].trackId.toString(),
+        urlImagen: data.results[0].artworkUrl100,
+        titulo: data.results[0].trackName,
+        autor: data.results[0].artistName,
+        descripcion: data.results[0].description,
+      };
+    });
+
+    this.http.obtenerListaEpisodios('1535809341').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
