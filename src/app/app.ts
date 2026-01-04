5cead3a15bbf176ca843505fe25a302de318e48b
01JuanK10 <camiloalzatebedoya15@gmail.com>
2026-01-04 13:37:27 -0500

    Actualizacion componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('PruebaTecnica');
+  router = inject(Router);
+  paginaPrincipal(){
+    this.router.navigate(['/']);
+  }
 }
